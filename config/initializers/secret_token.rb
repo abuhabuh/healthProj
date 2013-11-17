a729c70758a6f2c306e47ab5c0bb7e42f45f4fd9
John Wang <jue@alumni.cmu.edu>
2013-11-17 11:49:51 -0800

    First commit of empty shell proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HealthProj::Application.config.secret_key_base = '5020e134771d194183e9aa555ac435d1f056938e93c1e782be9d34a37c75398494c9e5035031d334029a2550c148fb986cf66a3d62cabdfa2d96bb9fc96c6e37'
